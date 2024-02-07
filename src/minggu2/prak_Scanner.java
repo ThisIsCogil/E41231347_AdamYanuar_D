7cd6e4845a67dd70b41e6665292ccc41f810373c
ThisIsCogil <yadam7195@gmail.com>
2024-02-07 22:03:37 +0700

    commit

---
+++ src/minggu2/prak_Scanner.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package minggu2;
+
+import java.util.Scanner;
+
+public class prak_Scanner {
+    
+    public static void main(String[] args) {
+        Scanner sl = new Scanner(System.in);
+        
+        System.out.println("Masukkan nilai1");
+    }
+    }
